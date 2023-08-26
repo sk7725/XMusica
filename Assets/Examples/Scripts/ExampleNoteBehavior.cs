7ee50806e00ebc0fb04ca5de9c622a7b389f1594
Sunny Kim <starwin@gmail.com>
2023-08-26 18:22:14 +0900

    1.3.1 added FromNoteString

--- Assets/Examples/Scripts/ExampleNoteBehavior.cs
+++ Assets/Examples/Scripts/ExampleNoteBehavior.cs
@@ -7,4 +7,15 @@ public class ExampleNoteBehavior : MonoBehaviour
 {
     [MIDINote]
     public int note, note2;
+
+    public string parse_note, parse_note2;
+
+    private void Start() {
+        Debug.Log($"Note: {XM_Utilities.GetNoteString(note)} ({note})");
+        Debug.Log($"Note2: {XM_Utilities.GetNoteString(note2)} ({note2})");
+        Debug.Log($"Parse Note: {XM_Utilities.FromNoteString(parse_note)}");
+        Debug.Log($"Parse Note2: {XM_Utilities.FromNoteString(parse_note2)}");
+        Debug.Assert(XM_Utilities.FromNoteString(XM_Utilities.GetNoteString(note)) == note);
+        Debug.Assert(XM_Utilities.GetNoteString(XM_Utilities.FromNoteString(parse_note)) == parse_note);
+    }
 }
--- Packages/XMusica/Scripts/Runtime/XM_Utilities.cs
+++ Packages/XMusica/Scripts/Runtime/XM_Utilities.cs
@@ -7,6 +7,10 @@ namespace XMusica {
     public static class XM_Utilities {
         private const string NOTE_STR = "CCDDEFFGGAAB";
 
+        /// <summary>
+        /// Returns the octave of the note. For example, it will return 0 for A0(21) and 9 for F#9(126).
+        /// </summary>
+        /// <param name="note">The note number as a midi note integer. (21-127; A0-G9)</param>
         public static int GetOctave(int note) {
             return note / 12 - 1;
         }
@@ -22,6 +26,50 @@ namespace XMusica {
             if (m == 1 || m == 3 || m == 6 || m == 8 || m == 10) sb.Append('#');
             sb.Append(n);
             return sb.ToString();
+        }
+
+        public static int FromNoteString(string note) {
+            if (note.Length < 2 || note.Length > 3) return -1;
+            //parse octave
+            int oct = (note.Length == 2 ? note[1] : note[2]) - '0';
+            if(oct < 0 || oct > 9) return -1;
+
+            //parse sharp
+            int offset = 0;
+            if (note.Length == 3) {
+                if (note[1] == '#') offset = 1;
+                else if (note[1] == 'b') offset = -1;
+                else return -1;
+            }
+
+            //parse note
+            int n;
+            switch (note.ToLower()[0]) {
+                case 'c':
+                    n = 0;
+                    break;
+                case 'd':
+                    n = 2;
+                    break;
+                case 'e':
+                    n = 4;
+                    break;
+                case 'f':
+                    n = 5;
+                    break;
+                case 'g':
+                    n = 7;
+                    break;
+                case 'a':
+                    n = 9;
+                    break;
+                case 'b':
+                    n = 11;
+                    break;
+                default: return -1;
+            }
+
+            return n + offset + (oct + 1) * 12;
         }
 
         public static int GetNoteSamplesRequired(VInstGenerationData data) {
