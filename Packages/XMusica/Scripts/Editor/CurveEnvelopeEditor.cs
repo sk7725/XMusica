924036589cc7ab73f0d946fa59ec4806dde2b82b
Sunny Kim <starwin@gmail.com>
2023-10-10 16:14:01 +0900

    curve envelope editor

--- Packages/XMusica/Scripts/Editor/CurveEnvelopeEditor.cs
+++ Packages/XMusica/Scripts/Editor/CurveEnvelopeEditor.cs
@@ -5,51 +5,51 @@ using UnityEditor;
 using XMusica.Envelope;
 
 namespace XMusica.EditorUtilities {
-    [CustomEditor(typeof(ADSREnvelope))]
-    public class ADSREditor : Editor {
+    [CustomEditor(typeof(CurveEnvelope))]
+    public class CurveEnvelopeEditor : Editor {
         #region GUIContent
         static readonly GUIContent t_settings = new GUIContent("Settings");
         static readonly GUIContent t_preview = new GUIContent("Preview");
 
-        static readonly GUIContent k_attack = new GUIContent("Attack", "The time it takes for the amplitude to peak after the key is pressed in seconds.");
-        static readonly GUIContent k_decay = new GUIContent("Decay", "The time it takes for the amplitude to transition to sustain as the key is held down in seconds.");
-        static readonly GUIContent k_sustain = new GUIContent("Sustain", "The sustain amplitude multiplier.");
-        static readonly GUIContent k_release = new GUIContent("Release", "The time it takes for the amplitude to fade out completely after the key is released in seconds.");
+        static readonly GUIContent k_pressCurve = new GUIContent("Press Curve", "The amplitude over time after the key is pressed.");
+        static readonly GUIContent k_pressDuration = new GUIContent("Press Curve Duration", "The length of the curve in seconds.");
+        static readonly GUIContent k_releaseCurve = new GUIContent("Release Curve", "The amplitude over time after the key is released. Multiplied by the last value of the press curve (the sustain value).");
+        static readonly GUIContent k_releaseDuration = new GUIContent("Release Curve Duration", "The time it takes for the amplitude to fade out completely.");
         #endregion
 
         private static float border = 3, previewHeight = 100, previewTapeHeight = 24, markerWidth = 9, markerHeight = 9;
         private static float minLabelWidth = 42, minTimeWidth = 23;
 
-        SerializedProperty s_attack, s_decay, s_sustain, s_release;
+        SerializedProperty s_pressCurve, s_pressDuration, s_releaseCurve, s_releaseDuration;
 
         private void OnEnable() {
-            s_attack = serializedObject.FindProperty("attack");
-            s_decay = serializedObject.FindProperty("decay");
-            s_sustain = serializedObject.FindProperty("sustain");
-            s_release = serializedObject.FindProperty("release");
+            s_pressCurve = serializedObject.FindProperty("pressCurve");
+            s_pressDuration = serializedObject.FindProperty("pressCurveDuration");
+            s_releaseCurve = serializedObject.FindProperty("releaseCurve");
+            s_releaseDuration = serializedObject.FindProperty("releaseCurveDuration");
         }
 
         public override void OnInspectorGUI() {
             serializedObject.Update();
 
             GUILayout.Label(t_preview, EditorStyles.boldLabel, GUILayout.Height(30));
-            GeneratePreview((ADSREnvelope)target);
+            GeneratePreview((CurveEnvelope)target);
 
             GUILayout.Space(5);
             GUILayout.Label(t_settings, EditorStyles.boldLabel, GUILayout.Height(30));
 
-            EditorGUILayout.PropertyField(s_attack, k_attack);
-            EditorGUILayout.PropertyField(s_decay, k_decay);
-            EditorGUILayout.PropertyField(s_sustain, k_sustain);
-            EditorGUILayout.PropertyField(s_release, k_release);
+            EditorGUILayout.PropertyField(s_pressCurve, k_pressCurve);
+            EditorGUILayout.PropertyField(s_pressDuration, k_pressDuration);
+            EditorGUILayout.PropertyField(s_releaseCurve, k_releaseCurve);
+            EditorGUILayout.PropertyField(s_releaseDuration, k_releaseDuration);
             serializedObject.ApplyModifiedProperties();
         }
 
         Rect graphicRect;
 
-        private void GeneratePreview(ADSREnvelope env) {
+        private void GeneratePreview(CurveEnvelope env) {
             float timeScale;
-            float maxTimeOccupy = Mathf.Max(env.attack + env.decay, env.release);
+            float maxTimeOccupy = Mathf.Max(env.pressCurveDuration, env.releaseCurveDuration);
             if (maxTimeOccupy < 0.22f) timeScale = 2f;
             else if (maxTimeOccupy < 0.45f) timeScale = 1f;
             else timeScale = 1 / (Mathf.Ceil(maxTimeOccupy + 0.1f) * 2);
@@ -67,7 +67,7 @@ namespace XMusica.EditorUtilities {
 
             //shading per 0.1
             Handles.color = new Color(0, 0, 0, 0.3f);
-            if(timeScale == 2f) {
+            if (timeScale == 2f) {
                 for (int i = 0; i < 3; i++) {
                     float f = i * 0.4f + 0.2f;
                     float f2 = i * 0.4f + 0.4f;
@@ -86,7 +86,7 @@ namespace XMusica.EditorUtilities {
 
             //draw timeline
             Handles.color = Color.white;
-            for(int i = 0; i * timeScale <= 10; i++) {
+            for (int i = 0; i * timeScale <= 10; i++) {
                 float l = i % 10 == 0 ? 9f : i % 5 == 0 ? 7f : 4f;
                 float x = i * 0.1f * timeScale;
                 Handles.DrawLine(GPos(x, 1), GPos(x, 1) + Vector2.up * l);
@@ -105,17 +105,36 @@ namespace XMusica.EditorUtilities {
                 Handles.Label(GPos(i * 0.1f + 0.5f, 0f) + Vector2.up * (newlineTimeContent && i % 2 == 0 ? 15f : 2f), s);
             }
 
-            //draw adsr
+            //draw curve
+            float sus = env.Sustain;
             Handles.color = Color.gray;
-            Handles.DrawDottedLine(GPos(0.5f, 1), GPos(0.5f, 1 - env.sustain), 0.2f);
+            Handles.DrawDottedLine(GPos(0.5f, 1), GPos(0.5f, 1 - sus), 0.2f);
+
+            //probably not the best way to draw a bezier curve, but i do not giveth a fuketh
             Handles.color = Color.red;
-            Handles.DrawLine(GPos(0, 1), GPos(env.attack * timeScale, 0));
-            Handles.DrawLine(GPos(env.attack * timeScale, 0), GPos((env.attack + env.decay) * timeScale, 1 - env.sustain));
-            Handles.DrawLine(GPos((env.attack + env.decay) * timeScale, 1 - env.sustain), GPos(0.5f, 1 - env.sustain));
+            int segments = 60;
+            Vector2 prev = GPos(0, 1 - env.DownAmplitude(0));
+            for(int i = 1; i <= segments; i++) {
+                float t = (i / (float)segments * 0.5f) / timeScale;
+                Vector2 next = GPos(i / (float)segments * 0.5f, 1 - env.DownAmplitude(t));
+                Handles.DrawLine(prev, next);
+                prev = next;
+            }
+
             Handles.color = Color.white;
-            Handles.DrawLine(GPos(0.5f, 1 - env.sustain), GPos(0.5f + timeScale * env.release, 1));
-            Handles.color = Color.gray;
-            Handles.DrawDottedLine(GPos(0.5f + timeScale * env.release, 1), GPos(1, 1), 0.2f);
+            prev = GPos(0.5f, 1 - env.UpAmplitude(0));
+            for (int i = 1; i <= segments; i++) {
+                float t = (i / (float)segments * 0.5f) / timeScale;
+                if(t >= env.releaseCurveDuration) {
+                    Handles.DrawLine(prev, GPos(i / (float)segments * 0.5f + 0.5f, 1));
+                    Handles.color = Color.gray;
+                    Handles.DrawDottedLine(GPos(i / (float)segments * 0.5f + 0.5f, 1), GPos(1, 1), 0.2f);
+                    break;
+                }
+                Vector2 next = GPos(i / (float)segments * 0.5f + 0.5f, 1 - env.UpAmplitude(t));
+                Handles.DrawLine(prev, next);
+                prev = next;
+            }
 
             GUI.backgroundColor = defColor;
         }
--- Packages/XMusica/Scripts/Runtime/Envelopes/CurveEnvelope.cs
+++ Packages/XMusica/Scripts/Runtime/Envelopes/CurveEnvelope.cs
@@ -5,23 +5,25 @@ using UnityEngine;
 namespace XMusica.Envelope {
     [CreateAssetMenu(fileName = "New Curve Envelope", menuName = "XMusica/Envelopes/Curve Envelope", order = 226)]
     public class CurveEnvelope : VirtualEnvelope {
-        [EnvelopeCurve(false)] public AnimationCurve pressCurve;
+        [EnvelopeCurve(false)] public AnimationCurve pressCurve = AnimationCurve.Linear(0, 0, 1, 1);
         [Range(0f, 3f)] public float pressCurveDuration = 0.5f;
-        [EnvelopeCurve(true)] public AnimationCurve releaseCurve;
+        [EnvelopeCurve(true)] public AnimationCurve releaseCurve = AnimationCurve.Linear(0, 1, 1, 0);
         [Range(0f, 5f)] public float releaseCurveDuration = 1f;
 
         public override float DownAmplitude(float timePassed) {
             if(timePassed < pressCurveDuration) {
                 return pressCurve.Evaluate(timePassed / pressCurveDuration);
             }
-            return pressCurve.Evaluate(1);
+            return pressCurve[pressCurve.length - 1].value;
         }
 
         public override float UpAmplitude(float timePassed) {
             if (timePassed < releaseCurveDuration) {
-                return releaseCurve.Evaluate(timePassed / releaseCurveDuration) * pressCurve.Evaluate(1);
+                return releaseCurve.Evaluate(timePassed / releaseCurveDuration) * pressCurve[pressCurve.length - 1].value;
             }
             return -1;
         }
+
+        public float Sustain => pressCurve[pressCurve.length - 1].value;
     }
 }
